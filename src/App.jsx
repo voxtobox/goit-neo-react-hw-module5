25367a8eebab2591d624433b52ae12918268caca
Volodymyr Antoniuk <voxtobox@gmail.com>
2025-01-07 21:53:25 -0800

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import './App.css';
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation/Navigation.jsx';
-import HomePage from './pages/HomePage/HomePage.jsx';
-import MoviesPage from './pages/MoviesPage/MoviesPage.jsx';
-import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage.jsx';
-import MovieReviews from './components/MovieReviews/MovieReviews.jsx';
-import MoviesCast from './components/MovieCast/MovieCast.jsx';
+
+const Navigation = lazy(() => import('./components/Navigation/Navigation.jsx'));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage.jsx'));
+const MovieDetailsPage = lazy(() =>
+  import('./pages/MovieDetailsPage/MovieDetailsPage.jsx')
+);
+const MovieReviews = lazy(() =>
+  import('./components/MovieReviews/MovieReviews.jsx')
+);
+const MovieCast = lazy(() => import('./components/MovieCast/MovieCast.jsx'));
 
 function App() {
   return (
     <>
       <Navigation></Navigation>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-          <Route path="reviews" element={<MovieReviews />} />
-          <Route path="cast" element={<MoviesCast />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="reviews" element={<MovieReviews />} />
+            <Route path="cast" element={<MovieCast />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </>
   );
 }
